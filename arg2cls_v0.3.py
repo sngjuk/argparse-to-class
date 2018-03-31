d42c6bce03460b00e05f3e409006920e0815a607
Seonguk Kim <suelcun92@gmail.com>
2018-03-31 13:03:09 +0900

    set_defaults error fix
    web server also updated for long arguments.

--- arg2cls_v0.3.py
+++ arg2cls_v0.3.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
+from collections import OrderedDict
 import sys
-
 def transform():
     if len(sys.argv) <2:
-        print('Usage : python arg2cls.py target.py target2.py(optional) ...')
+        print('Usage : python arg2cls.py [target.py] [target2.py(optional)] ...')
         sys.exit(0)
-
     sys.argv.pop(0)
     for fname in sys.argv:
-        if(__name__!='__main__'):
-            print(fname)
+        if(__name__!="__main__"):
+                print(fname)
         try :
             with open(fname, 'r') as f:
                 txt = f.read()
@@ -19,32 +17,39 @@ def transform():
                 if len(t) <2:
                     print('Nothing to make from file.')
                     sys.exit(0)
-
                 t = t[1].split('.parse_args')[0].split('add_argument')
                 t.pop(0)
-
-                name=[]
-                val=[]
+                argDct=OrderedDict()
                 for i, x in enumerate(t):
                     t =x.split('--')[1]
-                    name.append(t.split('\'')[0].replace('-','_'))
+                    aname =t.split('\'')[0].replace('-','_')
+                   
+                    argDct[aname]=''
                     dtype = ''
                     if('type' in t):
                         dtype = t.split('type=')[1].split(',')[0]
-
                     dfult = t.split('default=')
                     if len(dfult) <2:
-                        val.append('###manual_setting_required###')
+                        dfult=dfult[0].split('set_defaults(')
+
+                        if(len(dfult)>1):
+                          dfult=dfult[1]                          
+                          tn = dfult.split('=')[0]
+                          tv = dfult.split('=')[1].split(')')[0]
+                          argDct[tn]=tv
+
+                        else :
+                          argDct[aname]='###manual_setting_required###'
                     elif (dtype in ['int','float','long']):
-                        val.append(dfult[1].split(',')[0].replace('\'',''))
+                        argDct[aname]=dfult[1].split(',')[0].replace('\'','')
                     else:
-                        val.append(dfult[1].split(',')[0])
+                        argDct[aname]=dfult[1].split(',')[0]
 
                 print('')
                 print('class args:')
-                for i in zip(name,val):
-                    print('    ',end='')
-                    print(i[0],'=',i[1].replace('\"',''))
+                for i in argDct:
+                    print('  ',end='')
+                    print(i,'=',argDct[i].replace('\"',''))
                 print('')
 
         except IOError:
